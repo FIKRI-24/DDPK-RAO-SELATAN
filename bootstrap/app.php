0b117c5215b443a17adb0dfeb91c113bb07c5b66
FIKRI-24 <irkif1011@gmail.com>
2026-08-25 15:42:04 +0700

    feat: enable trustProxies for HTTPS reverse proxy on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'auth.guru' => \App\Http\Middleware\AuthGuru::class,
             'auth.siswa' => \App\Http\Middleware\AuthSiswa::class,
